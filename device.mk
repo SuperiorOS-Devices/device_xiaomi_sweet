fdf950f888b154c35d9897f6463153f2e1030c81
Roshan Pius <rpius@google.com>
2020-04-24 14:33:28 -0700

    sweet: Add SweetWifiOverlay Bug: 148617260 Test: Device boots up and connects to wifi networks. Change-Id: Iec27a6d947a232a42451ef095dc73d1a52327db5

--- device.mk
+++ device.mk
@@ -296,7 +296,9 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
-PRODUCT_ENFORCE_RRO_TARGETS += *
+# Overlay-RRO
+PRODUCT_PACKAGES += \
+    SweetWifiOverlay
 
 # Partitions
 PRODUCT_BUILD_SUPER_PARTITION := false
