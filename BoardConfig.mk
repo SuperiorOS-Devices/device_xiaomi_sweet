abbb87646b19ac14ef5e5e248f93a57053b4765f
Kuba Wojciechowski <nullbytepl@gmail.com>
2021-05-05 19:34:08 +0800

    sweet: Disable kpti
    It can also help increase performance by a lot in some scenarios.
    
    Change-Id: Ica465f586a44129047c266597316045fab762550
    Signed-off-by: Kuba Wojciechowski <nullbytepl@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -122,6 +122,7 @@ BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=a600000.dwc3
 BOARD_KERNEL_CMDLINE += service_locator.enable=1
 BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1
 BOARD_KERNEL_CMDLINE += loop.max_part=7
+BOARD_KERNEL_CMDLINE += kpti=off
 
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 BOARD_BOOTIMG_HEADER_VERSION := 2
