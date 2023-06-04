cd12bdec5203d9d038c1aa877b16e809c9bf82be
Arian <arian.kulmer@web.de>
2023-06-04 16:49:11 +0200

    sweet: Commonize fstab
    Change-Id: I99d91293aca9af3592ef41436d2334357b418226

--- device.mk
+++ device.mk
@@ -36,11 +36,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libsdmcore-shim
 
-# Fstab
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom \
-    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.qcom
-
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
