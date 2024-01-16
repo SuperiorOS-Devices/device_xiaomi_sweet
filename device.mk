ea7dca0659d85520fa81a56e682ff7018b1f05a9
unsatifsed27 <unsatifsederror@gmail.com>
2024-01-16 15:39:02 +0100

    sweet: [SQUASHED] Build more BT packages
    commit someone5678 <nemui3353@gmail.com>
    sweet: bt: Build libbthost_if_sink
    * We imported libbthost_if and renamed it to libbthost_if_sink
    
    sweet: Create dummy libldacBT_bco
    * This target is never existed and Bluetooth apex module also doesn't actually use it.
      But module still opening the library. Which cause log spam.
    
    * This commit is not required if you can just edit the source.
    
    Signed-off-by: unsatifsed27 <unsatifsederror@gmail.com>

--- device.mk
+++ device.mk
@@ -90,6 +90,12 @@ PRODUCT_COPY_FILES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
+    com.qualcomm.qti.bluetooth_audio@1.0.vendor \
+    libbtconfigstore \
+    libbthost_if_sink \
+    libldacBT_bco \
+    libldacBT_enc \
+    libldacBT_abr \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
