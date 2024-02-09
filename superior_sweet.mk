c52fd7967e5d6eb322aa5ef0decc2493dd962f9e
unsatifsed27 <unsatifsederror@gmail.com>
2024-02-09 17:13:32 +0700

    sweet: Bringup Superior

--- superior_sweet.mk
+++ superior_sweet.mk
@@ -11,10 +11,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from sweet device
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Superior stuff.
+MAINTAINER := unsatifsed27
+USE_QUICKPIC := true
+USE_DUCKDUCKGO := true
+USE_MOTO_CALCULATOR := true
+TARGET_BOOT_ANIMATION_RES := 1080
+$(call inherit-product, vendor/superior/config/common.mk)
 
-PRODUCT_NAME := lineage_sweet
+PRODUCT_NAME := superior_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 10 Pro
